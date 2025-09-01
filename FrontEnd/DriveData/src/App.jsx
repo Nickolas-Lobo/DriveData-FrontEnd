2f4564e11220396383a963618c7bb158eb00282d
Nickolas-Lobo <nickolas.lobo@outlook.com>
2025-09-01 20:45:29 -0300

    (config)Criação da tela de adicionar veiculo

--- FrontEnd/DriveData/src/App.jsx
+++ FrontEnd/DriveData/src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import PageLogin from './pages/PageLogin'
+import PageAdicionarCarro from './pages/PageAdicionarCarro'
 import PageCadastrarPeca from './pages/PageCadastrarPeca'
 
 function App() {
   return (
     <div className='appConfig'>
       <PageLogin/>
+      <PageAdicionarCarro/>
       <PageCadastrarPeca/>
     </div>
   )
