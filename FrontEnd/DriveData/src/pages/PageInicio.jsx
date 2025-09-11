c2202b57424aa9c1690e4e30ba3aa2ec96db5d31
Nickolas-Lobo <nickolas.lobo@outlook.com>
2025-09-11 16:19:30 -0300

    (update) Melhorias visuais na tela de inicio

--- FrontEnd/DriveData/src/pages/PageInicio.jsx
+++ FrontEnd/DriveData/src/pages/PageInicio.jsx
@@ -4,28 +4,76 @@ import React from 'react'
 const PageInicio = () => {
   return (
     <div className="backgroundInicio">
-      <div className="containerHead">
-        <div>
-          <img src="" alt="" />
-        </div>
-        <div>
-          <p>Modelo:</p>
-          <span></span>
-          <p>Quilometragem Atual:</p>
-          <span></span>
-          <p>Ultima Peça Trocada:</p>
-          <span></span>
+      <div className="linhaSuperior">
+        <div className="containerHead">
+          <div className="containerInfos">
+            <div className="imagemVeiculo">
+              <img src="" alt="Imagem do veículo" />
+            </div>
+            <div className="dadosVeiculo">
+              <p>Modelo:</p>
+              <span></span>
+              <p>Quilometragem Atual:</p>
+              <span></span>
+              <p>Última Peça Trocada:</p>
+              <span></span>
+            </div>
+          </div>
+          <button className="btnAdicionarPeca">Adicionar Manutenção</button>
         </div>
       </div>
-      <button className="btnAdicionarPeca">Adicionar Manutenção</button>
 
-      <div className="listaManutencoes">
-        <div className="titulo">
-          <h1>Minhas Manutenções</h1>
-        </div>
-        <div className="">
-          <p>Nome_peca</p>
-          <p>Vida util</p>
+      <div className="linhaInferior">
+        <div className="listaManutencoes">
+          <div className="titulo">
+            <h1>Minhas Manutenções</h1>
+          </div>
+          <div className="scrollWrapper">
+            <div className="containerCards">
+              <div className="itemManutencao">
+                <p>Nome da Peça</p>
+                <div className="imagemPeca">
+                  <img src="" alt="Imagem da peca" />
+                </div>
+                <p>Vida útil</p>
+              </div>
+              <div className="itemManutencao">
+                <p>Nome da Peça</p>
+                <div className="imagemPeca">
+                  <img src="" alt="Imagem da peca" />
+                </div>
+                <p>Vida útil</p>
+              </div>
+              <div className="itemManutencao">
+                <p>Nome da Peça</p>
+                <div className="imagemPeca">
+                  <img src="" alt="Imagem da peca" />
+                </div>
+                <p>Vida útil</p>
+              </div>
+              <div className="itemManutencao">
+                <p>Nome da Peça</p>
+                <div className="imagemPeca">
+                  <img src="" alt="Imagem da peca" />
+                </div>
+                <p>Vida útil</p>
+              </div>
+              <div className="itemManutencao">
+                <p>Nome da Peça</p>
+                <div className="imagemPeca">
+                  <img src="" alt="Imagem da peca" />
+                </div>
+                <p>Vida útil</p>
+              </div>
+              <div className="itemManutencao">
+                <p>Nome da Peça</p>
+                <div className="imagemPeca">
+                  <img src="" alt="Imagem da peca" />
+                </div>
+                <p>Vida útil</p>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
